4339b464e2eb34c65955335c6e35dba097958fe3
Alexis zhao <alexiszhao@192-168-1-126.tpgi.com.au>
2022-02-22 21:41:02 +1100

    new file

---
+++ workshop 1.R
@@ -0,0 +1,25 @@
+150/3
+a <- 10
+a
+
+b <- "cool"
+b
+c <- c(3,4,7,9)
+c
+
+summary(c)
+
+summary(c(3,4,7,9))
+
+install.packages("tidyverse")
+
+library(tidyverse)
+
+rm(b)
+
+data(mtcars)
+
+summary(mtcars)
+head(mtcars)
+
+glimpse(mtcars)
